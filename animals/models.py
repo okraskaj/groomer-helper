dd29c66c17875ab23d1e6e8785be15e6c72354e7
okraskaj <okraskaj@ee.pw.edu.pl>
2017-11-13 09:03:58 +0100

    Add Animal model

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from owners.models import Owner
+
+SIZE_CHOICES = [
+    ('miniature', 'Miniature'),
+    ('medium', 'Medium'),
+    ('large', 'Large'),
+    ('giant', 'Giant'),
+]
+
+GENDER_CHOICES = [
+    (True, 'Male'),
+    (False, 'Female'),
+]
+
+
+class Animal(models.Model):
+    name = models.TextField(max_length=100, blank=True, null=True)
+    gender = models.BooleanField(choices=GENDER_CHOICES)
+    species = models.TextField(max_length=50, blank=False, default='dog')
+    size = models.TextField(max_length=20, blank=True, choices=SIZE_CHOICES)
+    breed = models.TextField(max_length=20, blank=False, null=False)  # TODO: foreign key to Breeds
+    color = models.TextField(max_length=50, blank=True, null=True)
+    # service -dopytać
+    photo = models.ImageField()
+    motes = models.TextField(max_length=200, blank=True, null=True)
+    owner = models.ForeignKey(Owner)
