412a8733c333d712e609f1b9a07ed814ca88eeab
okraskaj <okraskaj@ee.pw.edu.pl>
2017-12-17 22:46:05 +0100

    SP-27,SP-19 Visit template and view

--- visits/views.py
+++ visits/views.py
@@ -11,7 +11,6 @@ class VisitListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['bookmark'] = 'visits'
         context['visits'] = Visit.objects.all()
         return context
 
@@ -22,5 +21,4 @@ class VisitDetailsView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['bookmark'] = 'visits'
         return context
