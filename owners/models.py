622bf00145ca8d5aa7014d5f6202941af223afe7
okraskaj <okraskaj@ee.pw.edu.pl>
2018-01-06 19:44:36 +0100

    fix owner recomendation system and to narrow content

--- owners/models.py
+++ owners/models.py
@@ -6,7 +6,7 @@ class Owner(models.Model):
     phone_number = models.TextField(max_length=20, blank=False, null=False)
     email = models.EmailField(max_length=100, blank=True, null=True)
     location = models.TextField(max_length=200, blank=True, null=True)
-    recommended_by = models.ManyToManyField('self', blank=True, symmetrical=False)
+    recommended_by = models.ForeignKey('Owner', null=True, on_delete=models.CASCADE)
     # field to reward people with most recomendations as well as to remember people correctly
     # TODO: integrate with facebook - load image picture on creation and load additional data (likes?).
 
