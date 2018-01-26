01e32a8ce55e28bf51d21b0ee92f274d6895282a
Jan <okraskaj@ee.pw.edu.pl>
2018-01-26 01:06:45 +0100

    exit from debug mode

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's3tlyfm)th@f@k*1ns3s*w%vlh^#8qwd#!w6xrkk-g0^p+mlof'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
